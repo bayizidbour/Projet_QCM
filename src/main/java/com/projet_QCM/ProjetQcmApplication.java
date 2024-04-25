7e76601cc6ad7e27254a2fc55c44ce9891a5620f
BOURAIMA <bayizidbour@gmail.com>
2024-04-25 10:58:04 +0200

    mise en place du projet QCM

---
+++ src/main/java/com/projet_QCM/ProjetQcmApplication.java
@@ -0,0 +1,13 @@
+package com.projet_QCM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetQcmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetQcmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projet_QCM/ProjetQcmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projet_QCM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetQcmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
