03fac88f00f32b61c739deedf7db4903ec1ee81c
massire <massire.org@gmail.com>
2024-04-26 12:52:17 +0200

    create entity question,moyenne and optionQuiz

---
+++ src/main/java/com/projet_QCM/Question.java
@@ -0,0 +1,26 @@
+package com.projet_QCM;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_question;
+
+    private String libelle;
+
+    @Enumerated(EnumType.STRING)
+    private TypeQuestion typeQuestion;
+
+    private double point;
+
+}
