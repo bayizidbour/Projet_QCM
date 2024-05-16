32ceac5681101d7cb352d32d114d6c38fb15c7df
BOURAIMA <bayizidbour@gmail.com>
2024-05-16 17:27:45 +0200

    modification de la partie Quiz

--- src/main/java/com/projet_QCM/controller/QuizController.java
+++ src/main/java/com/projet_QCM/controller/QuizController.java
@@ -1,5 +1,8 @@
 package com.projet_QCM.controller;
 
+import java.time.LocalDate;
+import java.util.Optional;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -9,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.projet_QCM.model.Quiz;
 import com.projet_QCM.service.QuizServiceImpl;
@@ -20,78 +24,117 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class QuizController {
 
-    public final QuizServiceImpl quizService;
+	public final QuizServiceImpl quizService;
 
-    // Afficher la liste des quiz
-    
-    @GetMapping("/admin/quiz/list")
-    public String getAllQuiz(Model model){
-        model.addAttribute("quizs",quizService.getAll());
-        return "quiz/list";
-    }
-    
-    // Ajouter un quiz
-   
-    @GetMapping("/admin/quiz/add")
-    public String addQuiz(Model model){
-    	model.addAttribute("quiz", new Quiz());
-        return "quiz/index";
-    }
-    
-    //Afficher les quiz à passer
-    
-    @GetMapping("/user/quiz/done")
-    public String doneQuiz(Model model){ 
-    	model.addAttribute("quizs", quizService.getAll());
-        return "eleve/index";
-    }
-    
-    // Soumettre quiz
-    @GetMapping("/user/quiz/done/examen/{id}")
-    public String examenQuiz(Model model,@PathVariable Long id){  
-    	
-        return "eleve/examen";
-    }
-    
-    // Confirmer soumission Quiz
-    @GetMapping("/user/quiz/done/examen/confirm")
-    public String confirmationQuiz(Model model){   	
-    	return "eleve/confirm";
-    }
-    
-    // Afficher note QUiz
-   
-    @GetMapping("/user/quiz")
-    public String insererQuiz(Model model){
-        return "eleve/note";
-    }
-    
-    //Consulter Moyenne Quiz à faire
-    
-    @GetMapping("/user/quiz/moyenne")
-    public String moyenneQUiz(Model model){
-        return "eleve/moyenne";
-    }
-    
-    
-    
-    // --------------------------------------------
-    @GetMapping("/{id}")
-    public String getById(@PathVariable Long id){
-        quizService.getById(id);
-        return "";
-    }
+	// Afficher la liste des quiz
 
-    @DeleteMapping("/delete/{id}")
-    public String delete(@PathVariable Long id) {
-        quizService.delete(id);
-        return "";
-    }
+	@GetMapping("/admin/quiz/list")
+	public String getAllQuiz(Model model) {
+		model.addAttribute("quizs", quizService.getAll());
+		
+		return "quiz/list";
+	}
 
-    @PutMapping("/update/{id}")
-    public String update(Quiz quiz, @PathVariable Long id){
-        quizService.delete(id);
-        return "";
-    }
+	// Ajouter un quiz
+
+	@GetMapping("/admin/quiz/add")
+	public String addQuiz(Model model) {
+		model.addAttribute("quiz", new Quiz());
+		model.addAttribute("date_now", LocalDate.now());
+
+		return "quiz/index";
+	}
+
+	// Créer et ajouter un quiz dans la base de donnée
+	@PostMapping("/admin/quiz/inserer")
+	public String createQuiz(@Valid Quiz quiz, BindingResult result, RedirectAttributes ra) {
+
+		if (result.hasErrors()) {
+			return "quiz/index";
+		}
+		System.out.println("dehors" + quiz.getId());
+		if (quiz.getId() != null) {
+			System.out.println("dedans" + quiz.getId());
+			quizService.update(quiz, quiz.getId());
+			ra.addFlashAttribute("success", "Le quiz est modifié avec success");
+		} else {
+			System.out.println("update Non ok");
+			quizService.create(quiz);
+			ra.addFlashAttribute("success", "Le quiz est ajouté avec success");
+		}
+		return "redirect:/admin/quiz/list";
+	}
+
+//		// Créer et ajouter un quiz dans la base de donnée
+//				@PostMapping("/admin/quiz/update")
+//				public String updateQuiz(@Valid Quiz quiz, BindingResult result, RedirectAttributes ra) {
+//					
+//					if(result.hasErrors()) {
+//						return "quiz/index";
+//					}
+//					
+//					if(quiz.getId() != 0) {
+//						quizService.update(quiz, quiz.getId());
+//						ra.addFlashAttribute("success", "Le quiz est modifié avec success");
+//					}
+//					return "redirect:/admin/quiz/list";
+//				}
+	// Afficher les quiz à passer
+
+	@GetMapping("/user/quiz/done")
+	public String doneQuiz(Model model) {
+		model.addAttribute("quizs", quizService.getAll());
+		model.addAttribute("date_now", LocalDate.now());
+		return "eleve/index";
+	}
+
+	// Soumettre quiz
+	@GetMapping("/user/quiz/done/examen")
+	public String examenQuiz(Model model) {
+
+		return "eleve/examen";
+	}
+
+	// Confirmer soumission Quiz
+	@GetMapping("/user/quiz/done/examen/confirm")
+	public String confirmationQuiz(Model model) {
+		return "eleve/confirm";
+	}
+
+	// Afficher note QUiz
+
+	@GetMapping("/user/quiz")
+	public String insererQuiz(Model model) {
+		return "eleve/note";
+	}
+
+	// Consulter Moyenne Quiz
+
+	@GetMapping("/user/quiz/moyenne")
+	public String moyenneQUiz(Model model) {
+		return "eleve/moyenne";
+	}
+
+	// --------------------------------------------
+	@GetMapping("/{id}")
+	public String getById(@PathVariable Long id) {
+		quizService.getById(id);
+		return "";
+	}
+
+	@GetMapping("/quiz/delete/admin/{id}")
+	public String delete(@PathVariable Long id) {
+		quizService.delete(id);
+		return "redirect:/admin/quiz/list";
+	}
+
+	@GetMapping("/quiz/update/admin/{id}")
+	public String update(Model model, @PathVariable Long id, RedirectAttributes ra) {
+
+		model.addAttribute("quiz", quizService.getById(id));
+		model.addAttribute("date_now", LocalDate.now());
+		return "quiz/index";
+
+	}
 
 }
