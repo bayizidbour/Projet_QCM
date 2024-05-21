8121078fde2457b2ba592a2930184e6cc696c6c8
BOURAIMA <bayizidbour@gmail.com>
2024-05-21 10:52:55 +0200

    remplacement du type Integer par Faire_Key dans le repository FaireRepository

--- src/main/java/com/projet_QCM/repository/FaireRepository.java
+++ src/main/java/com/projet_QCM/repository/FaireRepository.java
@@ -1,12 +1,14 @@
 package com.projet_QCM.repository;
 
 import com.projet_QCM.model.Faire;
+import com.projet_QCM.model.Faire_key;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FaireRepository extends JpaRepository<Faire, Integer> {
+public interface FaireRepository extends JpaRepository<Faire, Faire_key> {
 
     @Query(value = "SELECT AVG(note) FROM faire WHERE user_id=?;",nativeQuery = true)
     double calculMoyenneByIdUser(int id);
